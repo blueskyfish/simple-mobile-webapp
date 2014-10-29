6f3c32dca5b8290344008b146ff5814b89ece63a
Mulder3 <mulder3@kirchnerei.de>
2014-10-29 21:21:41 +0100

    Add popover
    - home view with sidebar (popover)
    - jQuery

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,6 +92,7 @@ module.exports = function (grunt) {
 						'bower_components/ratchet/dist/css/ratchet.min.css'
 					],
 					'dest/assets/js/vendor.min.js': [
+						'bower_components/jquery/dist/jquery.min.js',
 						'bower_components/angular/angular.min.js',
 						'bower_components/angular-route/angular-route.min.js',
 						'bower_components/angular-translate/angular-translate.min.js',
